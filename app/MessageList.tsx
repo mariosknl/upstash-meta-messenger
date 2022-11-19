7eaaf4388e47156471bd54f02aae77d9c30ec5b1
mariosknl <mariosknl@outlook.com>
2022-11-19 08:31:12 +0200

    building the chat input component 1/4

---
+++ app/MessageList.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MessageList() {
+	return <div>Messages go here...</div>;
+}
+
+export default MessageList;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import ChatInput from "./ChatInput";
+import MessageList from "./MessageList";
 
 function HomePage() {
 	return (
-		<div>
-			<h1>Welcome to the MESSENGER</h1>
-		</div>
+		<main>
+			<MessageList />
+			<ChatInput />
+		</main>
 	);
 }
 
